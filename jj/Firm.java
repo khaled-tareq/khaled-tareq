a06c1e972f8949677a039adc7746b0810f91d6f9
khaled-tareq <khaledtareq2006@gmail.com>
2025-06-09 12:54:32 +0300

    Delete jj directory

--- jj/Firm.java
+++
@@ -1,7 +0,0 @@
-public class Firm {
-    public static void main(String[] args) {
-
-        staff s= new staff();
-        s.payday();
-    }
-}
--- jj/Volunteer.java
+++
@@ -1,8 +0,0 @@
-public class Volunteer  extends StaffMember{
-    public Volunteer(String Name, String Addres, String phone){
-        super(Name, Addres, phone);
-    }
-    public double pay(){
-        return 0.0;
-    }
-}
